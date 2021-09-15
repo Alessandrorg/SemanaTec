8c9f7bdd72fe429790cd5879b232d94a616913c9
Alessandrorg <75130621+Alessandrorg@users.noreply.github.com>
2021-09-15 11:27:48 -0500

    Add files via upload

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,108 +1,122 @@
-import pygame
+import pygame, random
+
+ANCHO = 900
+ALTO = 585
+BLANCO = (255, 255, 255)
+ROJO = (255, 0, 0)
+
+def texto(texto, tam=20, color=(0, 0, 0)):
+    fuente = pygame.font.Font(None, tam)
+    return fuente.render(texto, True, color)
+
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("C:/Users/Aless/OneDrive/Escritorio/laser.png").convert()
+        self.image.set_colorkey(negro)
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.y -= 5
+
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('C:/Users/Aless/OneDrive/Escritorio/inv2.png').convert()
+        self.image.set_colorkey(negro)
+        self.rect = self.image.get_rect()
+    
+    def update(self):
+        self.rect.x -= 2
+        
+        if self.rect.x < 0:
+            self.rect.x = 900 
+   
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('C:/Users/Aless/OneDrive/Escritorio/nave2.png').convert()
+        self.image.set_colorkey(negro)
+        self.rect = self.image.get_rect()
+    
+    def  update(self):
+        mouse_pos = pygame.mouse.get_pos()
+        self.rect.x = mouse_pos[0]
+        self.rect.y = 450
+
+    
+negro = 0,0,0
+blanco = 255,255,255
 
 def main():
-    pygame.init() 
-    size = 800,600 
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Mi primer juego")
+    pygame.init()
+    screen = pygame.display.set_mode([900,585])
 
-    width, height = 800,600
-    speed = [2, 2]
-    white = 255, 255, 255
-    barrasize = 20, 150
-    ballsize = 50,50
-    mitad_x = width/2-ballsize[0]/2
-    mitad_y = height/2-ballsize[1]/2
-    score = [0, 0]
-
-    fuente = pygame.font.Font(None, 50)
-    marcador = fuente.render("Score", True, (0, 0, 0))
-    puntaje1 = fuente.render(str(score[0]), True, (0,0,0))
-    puntaje2 = fuente.render(str(score[1]), True, (0,0,0))
-
-    ball = pygame.image.load('ball.png')
-    ball = pygame.transform.scale(ball, ballsize)
-    ballrect = ball.get_rect();
-    ballrect.move_ip(mitad_x, mitad_y)
-
- 
-    barra = pygame.image.load('bar.jpg')
-    barra = pygame.transform.scale(barra, (barrasize))
-    barrarect = barra.get_rect()
-
-   
-    barrarect.move_ip(50,height/2-barrasize[1]/2)
-        
-    barra2 = pygame.image.load('bar2.jpg')
-    barra2 = pygame.transform.scale(barra2, (barrasize))
-    barrarect2 = barra2.get_rect()
-
-    #se ubica la barra a la mita de la ventana
-    barrarect2.move_ip(width-50,height/2-barrasize[1]/2)
-
+    clock = pygame.time.Clock()
     run = True
+    score = 0
+
+    fondo = pygame.image.load('C:/Users/Aless/OneDrive/Escritorio/Space.jpg')
+    fondorect = fondo.get_rect();
+
+    all_sprite_list = pygame.sprite.Group()
+    meteor_list = pygame.sprite.Group()
+    laser_list = pygame.sprite.Group()
+
+    for i in range(20):
+        meteor = Meteor()
+        meteor.rect.x = random.randrange(800)
+        meteor.rect.y = random.randrange(400)
+
+        meteor_list.add(meteor)
+        all_sprite_list.add(meteor)
+
+    player = Player()
+    all_sprite_list.add(player)
+
     while run:
-        pygame.time.delay(5) #delay que contralará la velocidad            
-        
         for event in pygame.event.get(): #se captura el evento que se produce
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                laser = Laser()
+                laser.rect.x = player.rect.x + 90
+                laser.rect.y = player.rect.y - 20
 
-        #se detecata si se ha pulsado alguna tecla
-        keys = pygame.key.get_pressed()
-
-
-        if keys[pygame.K_w] and barrarect.top > 0:
-            barrarect = barrarect.move(0, -2)
-
-        if keys[pygame.K_s] and barrarect.bottom < height:
-            barrarect = barrarect.move(0, 2)
-
-        if keys[pygame.K_UP] and barrarect2.top > 0:
-            barrarect2 = barrarect2.move(0, -2)
-
-        if keys[pygame.K_DOWN] and barrarect2.bottom < height:
-            barrarect2 = barrarect2.move(0, 2)
-
-        #se detemina si hay colisiones 
-        if barrarect.colliderect(ballrect):
-            speed[0] = -speed[0]
-        if barrarect2.colliderect(ballrect):
-            speed[0] = -speed[0]
-
-        ballrect = ballrect.move(speed) #se mueve el objeto
-        #se determinan los límites del objeto
-        if ballrect.left < 0 or ballrect.right > width:
-        	multiplicador = 1
-        	if(ballrect[1]>mitad_y):
-        		multiplicador = -1
-
-        	if ballrect.left < 0:
-        		score[0] += 1
-        		puntaje2 = fuente.render(str(score[0]), True, (0,0,0))
-        	if ballrect.right > width:
-        		score[1] = score[1]+1
-        		puntaje1 = fuente.render(str(score[1]), True, (0,0,0))
-    		
-        	ballrect = ballrect.move(-1*ballrect[0]+mitad_x, mitad_y+multiplicador*ballrect[1])
-        	pygame.time.delay(800)
-
+                all_sprite_list.add(laser)
+                laser_list.add(laser)
+    
+        all_sprite_list.update() #actualiza el movimiento de los sprite
+        #se definen las colisiones
+        for laser in laser_list:
+            meteor_hit_list = pygame.sprite.spritecollide(laser, meteor_list, True) # el true es para que desaparezcan
+            #para eliminar el laser en las colisiones
+            for meteor in meteor_hit_list:
+                all_sprite_list.remove(laser)
+                laser_list.remove(laser)
+                score += 1
+                print(score)
+            if laser.rect.y < 0:
+                all_sprite_list.remove(laser)
+                laser_list.remove(laser)
         
+        #screen.fill(blanco)
+        screen.blit(fondo, fondorect)
+        all_sprite_list.draw(screen) #se pintan todos los sprites
+        screen.blit(texto("Score:", 60, BLANCO), (ANCHO / 3, 10))
+        screen.blit(texto(str(score), 60, BLANCO), (ANCHO / 2, 10))
 
-        if ballrect.top < 0 or ballrect.bottom > height:
-            speed[1] = -speed[1]
 
-
-        #se borra la pantalla anterior con el fondo blanco
-        screen.fill(white)
-        screen.blit(marcador, (width/2-50, 50))
-        screen.blit(puntaje1, (width/2-100, 50))
-        screen.blit(puntaje2, (width/2+100, 50))
-        screen.blit(ball, ballrect)
-        screen.blit(barra,barrarect)
-        screen.blit(barra2,barrarect2)
         pygame.display.flip()
-    pygame.quit() #se termina el juego
+        clock.tick(60)
+        
+    pygame.quit()
+
+
 
 if __name__ == "__main__":
     main()
